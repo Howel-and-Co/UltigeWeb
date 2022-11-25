ed924158b554d81df8e89432763d325b8e04734a
Jonathan Owen Kuntjoro <53405940+jowenzero@users.noreply.github.com>
2022-11-25 18:16:13 +0700

    20221125_JOW_1
    1. Fix & bug on query params

--- pages/change-password/index.js
+++ pages/change-password/index.js
@@ -72,9 +72,6 @@ const ChangePassword = () => {
         const result = await axios({
             method: 'post',
             url: 'https://api.ultige.com/ultigeapi/web/password/changepassword',
-            headers: {
-                "Client-ID": '8572d3838a0e2b9b4de63e6c72e3ab5d'
-            },
             data: {
                 username: newUsername, 
                 oldPassword: sha256(values.oldPassword),
@@ -93,9 +90,6 @@ const ChangePassword = () => {
         const result2 = await axios({
             method: 'post',
             url: 'https://api.ultige.com/ultigeapi/authenticate',
-            headers: {
-                "Client-ID": '8572d3838a0e2b9b4de63e6c72e3ab5d'
-            },
             data: {
                 username: newUsername, 
                 password: sha256(values.confirmNewPassword)
